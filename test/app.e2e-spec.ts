eae74560a776dd528ea38742702df5a45652a217
Robbie <hemi33379@gmail.com>
2024-06-08 20:17:50 +0900

    post 400 / patch, delete 200 test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -36,7 +36,7 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -45,6 +45,17 @@ describe('AppController (e2e)', () => {
           genres: ['Test'],
         })
         .expect(201);
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2024,
+          genres: ['Test'],
+          hacked: 'by me',
+        })
+        .expect(400);
     });
 
     it('DELETE', () => {
@@ -59,7 +70,16 @@ describe('AppController (e2e)', () => {
     it('GET 404', () => {
       return request(app.getHttpServer()).get('/movies/999').expect(404);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({
+          title: 'Updated Test',
+        })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
