c7ede5109bbf428155811098b89e533916e57010
hemi333 <71807433+hemi333@users.noreply.github.com>
2024-07-29 14:46:56 +0900

    init

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,26 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { title } from 'process';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    // test환경에서도 실제 어플리케이션 환경을 적용해줘야 함
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-      }),
-    );
     await app.init();
   });
 
@@ -28,58 +19,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Welcome to my Movie API');
-  });
-
-  describe('/movies', () => {
-    it('GET', () => {
-      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
-    });
-
-    it('POST 201', () => {
-      return request(app.getHttpServer())
-        .post('/movies')
-        .send({
-          title: 'Test',
-          year: 2024,
-          genres: ['Test'],
-        })
-        .expect(201);
-    });
-    it('POST 400', () => {
-      return request(app.getHttpServer())
-        .post('/movies')
-        .send({
-          title: 'Test',
-          year: 2024,
-          genres: ['Test'],
-          hacked: 'by me',
-        })
-        .expect(400);
-    });
-
-    it('DELETE', () => {
-      return request(app.getHttpServer()).delete('/movies').expect(404);
-    });
-  });
-
-  describe('/movies/:id', () => {
-    it('GET 200', () => {
-      return request(app.getHttpServer()).get('/movies/1').expect(200);
-    });
-    it('GET 404', () => {
-      return request(app.getHttpServer()).get('/movies/999').expect(404);
-    });
-    it('PATCH 200', () => {
-      return request(app.getHttpServer())
-        .patch('/movies/1')
-        .send({
-          title: 'Updated Test',
-        })
-        .expect(200);
-    });
-    it('DELETE 200', () => {
-      return request(app.getHttpServer()).delete('/movies/1').expect(200);
-    });
+      .expect('Hello World!');
   });
 });
