4a4b4ac6a665bcab1378aabe7edc89a04c0f6696
hemi333 <71807433+hemi333@users.noreply.github.com>
2024-07-30 14:57:33 +0900

    회원가입(Sign Up)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { CredentialsDto } from './credential.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private userRepository: UserRepository) {}
+
+  async signUp(credentialDto: CredentialsDto): Promise<void> {
+    const { username, password } = credentialDto;
+
+    // hash and salt password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    console.log('salt', salt);
+    console.log('hashedPassword', hashedPassword);
+
+    const user = this.userRepository.create({
+      username,
+      password: hashedPassword,
+    });
+
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 }
